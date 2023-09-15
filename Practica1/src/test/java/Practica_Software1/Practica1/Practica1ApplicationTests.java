b45e5e7b9c894d734c57fc86e16f0d80525146bc
@eco-intern <git-intern@econotec.com.bo>
2023-09-14 20:12:18 -0400

    Subiendo Practica

---
+++ Practica1/src/test/java/Practica_Software1/Practica1/Practica1ApplicationTests.java
@@ -0,0 +1,13 @@
+package Practica_Software1.Practica1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Practica1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
